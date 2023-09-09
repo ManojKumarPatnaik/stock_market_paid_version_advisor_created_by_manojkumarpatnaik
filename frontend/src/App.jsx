0a9feebe4926933d7c90eb9e8b0941bccccd0bf2
pengwenlei <pengwenlei@mindray.com>
2023-09-10 01:48:03 +0530

    css changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,47 +1,45 @@
-import { useState } from 'react'
-import './App.css'
-import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
+import React from "react";
+
+import { useState } from "react";
+import "./App.css";
+import "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
 import axios from "axios";
-import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from '@chatscope/chat-ui-kit-react';
-
-
-// "Explain things like you would to a 10 year old learning how to code."
-const systemMessage = { //  Explain things like you're talking to a software professional with 5 years of experience.
-  "role": "system", "content": "Explain things like you're talking to a software professional with 2 years of experience."
-}
+import {
+  MainContainer,
+  ChatContainer,
+  MessageList,
+  Message,
+  MessageInput,
+  TypingIndicator,
+} from "@chatscope/chat-ui-kit-react";
 
 function App() {
+  const base64regex = /^([0-9a-zA-Z+/]{4})*(([0-9a-zA-Z+/]{2}==)|([0-9a-zA-Z+/]{3}=))?$/;
   const [messages, setMessages] = useState([
     {
       message: "Hello, I'm Jarvis StockBot!",
       sentTime: "just now",
-      sender: "JarvisStockbot"
-    }
+      sender: "JarvisStockbot",
+    },
   ]);
   const [isTyping, setIsTyping] = useState(false);
 
   const handleSend = async (message) => {
     const newMessage = {
       message,
-      direction: 'outgoing',
-      sender: "user"
+      direction: "outgoing",
+      sender: "user",
     };
 
     const newMessages = [...messages, newMessage];
 
     setMessages(newMessages);
 
-    // Initial system message to determine JarvisStockbot functionality
-    // How it responds, how it talks, etc.
     setIsTyping(true);
     await processMessageToJarvisStockbot(newMessages);
   };
 
-  async function processMessageToJarvisStockbot(chatMessages) { // messages is an array of messages
-    // Format messages for JarvisStockbot API
-    // API is expecting objects in format of { role: "user" or "assistant", "content": "message here"}
-    // So we need to reformat
-
+  async function processMessageToJarvisStockbot(chatMessages) {
     let apiMessages = chatMessages.map((messageObject) => {
       let role = "";
       if (messageObject.sender === "user") {
@@ -49,40 +47,40 @@ function App() {
       } else {
         role = "assistant2";
       }
-      return { role: role, content: messageObject.message }
+      return { role: role, content: messageObject.message };
     });
 
-    const message= apiMessages[apiMessages.length - 1]
+    const message = apiMessages[apiMessages.length - 1];
 
-
-    // Get the request body set up with the model we plan to use
-    // and the messages which we formatted above. We add a system message in the front to'
-    // determine how we want JarvisStockbot to act. 
     const apiRequestBody = {
-      "model": "gpt-3.5-turbo",
-      "messages": [  
-        message // The messages from our chat with JarvisStockbot
-      ]
-    }
+      model: "gpt-3.5-turbo",
+      messages: [message],
+    };
 
-    axios.post("http://localhost:5000/jarvis/openai",
-      apiRequestBody
-    ).then((data) => {
-      return data.data;
-    }).then((data) => {
-      console.log(data);
-      setMessages([...chatMessages, {
-        message: data.chart,
-        sender: "JarvisStockbot"
-      }]);
-      setIsTyping(false);
-    }).catch(error => {
-      setMessages([...chatMessages, {
-        message: "Currently cant serve your request please try later!",
-        sender: "JarvisStockbot"
-      }])
-      console.log(error);
-    });;
+    axios
+      .post("http://localhost:5000/jarvis/openai", apiRequestBody)
+      .then((data) => {
+        return data.data;
+      })
+      .then((data) => {
+        setMessages([
+          ...chatMessages,
+          {
+            message: data.chart,
+            sender: "JarvisStockbot",
+          },
+        ]);
+        setIsTyping(false);
+      })
+      .catch((error) => {
+        setMessages([
+          ...chatMessages,
+          {
+            message: "Currently cant serve your request please try later!",
+            sender: "JarvisStockbot",
+          },
+        ]);
+      });
   }
 
   return (
@@ -92,27 +90,33 @@ function App() {
           <ChatContainer>
             <MessageList
               scrollBehavior="smooth"
-              typingIndicator={isTyping ? <TypingIndicator content="typing" /> : null}
+              typingIndicator={
+                isTyping ? <TypingIndicator content="typing" /> : null
+              }
             >
               {messages.map((message, i) => {
-
-
-                // console.log(message)
-                // return <div className='message'>{
-                //   message.sender === "JarvisStockbot" ?
-                //     <img src='src\assets\bot.png'></img> : <img src='src\assets\user.png'></img>}
-                //   <Message key={i} model={message} /></div>
-
-                console.log(message)
-                return <>{message.sender === "JarvisStockbot" ?
-                  <div className='message'>
-                    <img src='src\assets\bot.png'></img>
-                    <img src={`data:image/png;base64,${message.message}`}/></div> : <div className='message-user'>
-                    <Message key={i} model={message} />
-                    <img src='src\assets\user.png'></img>
-                  </div>}</>
-
-
+                return (
+                  <>
+                    {message.sender === "JarvisStockbot" ? (
+                      <div className="message">
+                        {base64regex.test(message.message) ? (
+                          <img
+                            src={`data:image/png;base64,${message.message}`}
+                          />
+                        ) : (
+                          <>
+                            <img src="src\assets\bot.png"></img>
+                            <div className="message">{message.message}</div>
+                          </>
+                        )}
+                      </div>
+                    ) : (
+                      <div className="message-user">
+                        <Message key={i} model={message} />
+                      </div>
+                    )}
+                  </>
+                );
               })}
             </MessageList>
             <MessageInput placeholder="Type message here" onSend={handleSend} />
@@ -120,7 +124,7 @@ function App() {
         </MainContainer>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
